6b95a89d2bf12c2770a9c18fa70a3d2169173677
yanis009 <romanyanishdesign@gmail.com>
2021-08-14 14:48:08 -0500

    added hamburger menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 
 console.log("Hello World")
+
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');
+   if (x.className === 'navtoggle') {
+   x.className += ' responsive';
+ } else {
+   x.className = 'navtoggle';
+ }
+}
